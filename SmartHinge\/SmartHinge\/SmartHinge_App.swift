e785ea2e01e3a04355eb4521150ec34d9fb9469f
Amogh <amogh.biradar@gmail.com>
2026-02-05 13:12:11 -0500

    Base structure for App (everything is breaking)

---
+++ SmartHinge\/SmartHinge\/SmartHinge_App.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHinge_App.swift
+//  SmartHinge\
+//
+//  Created by AmoghB on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct SmartHinge_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
