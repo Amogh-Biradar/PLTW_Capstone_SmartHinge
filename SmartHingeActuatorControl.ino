ec65909598b026141d18ecf50312d211cb651d38
Amogh biradar <83672871+Amogh-Biradar@users.noreply.github.com>
2026-03-19 14:07:05 -0400

    added hm-10 code to test later (first gotta make sure hm-10 works)
    Added Bluetooth control for actuator with commands to extend, contract, and stop.

--- SmartHingeActuatorControl.ino
+++ SmartHingeActuatorControl.ino
@@ -20,3 +20,34 @@ void loop() {
   move(0, 0); delay(500);
   
 }
+/***
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define PIN_A 5
+#define PIN_B 6
+#define BT_RX 10
+#define BT_TX 11
+
+SoftwareSerial bt(BT_RX, BT_TX);
+
+void move(bool a, bool b) {
+  digitalWrite(PIN_A, !a);
+  digitalWrite(PIN_B, !b);
+}
+
+void setup() {
+  pinMode(PIN_A, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+  bt.begin(9600);
+}
+
+void loop() {
+  if (bt.available()) {
+    char cmd = bt.read();
+    if (cmd == 'E') move(1, 0);      // Extend
+    else if (cmd == 'C') move(0, 1); // Contract
+    else if (cmd == 'S') move(0, 0); // Stop
+  }
+}
+***/
