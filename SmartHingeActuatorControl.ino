686b734ee4e02fac44720bd898dec80727cead5d
Amogh biradar <83672871+Amogh-Biradar@users.noreply.github.com>
2026-03-21 19:48:37 -0400

    Integrate ESP8266 WiFi and web server functionality

--- SmartHingeActuatorControl.ino
+++ SmartHingeActuatorControl.ino
@@ -21,33 +21,57 @@ void loop() {
   
 }
 /***
-#include <Arduino.h>
-#include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-#define PIN_A 5
-#define PIN_B 6
-#define BT_RX 10
-#define BT_TX 11
+const char* ssid     = "TP_Link";
+const char* password = "65970324";
 
-SoftwareSerial bt(BT_RX, BT_TX);
-
-void move(bool a, bool b) {
-  digitalWrite(PIN_A, !a);
-  digitalWrite(PIN_B, !b);
-}
+ESP8266WebServer server(80);
+String lastReceived = "Nothing yet.";
 
 void setup() {
-  pinMode(PIN_A, OUTPUT);
-  pinMode(PIN_B, OUTPUT);
-  bt.begin(9600);
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected! Go to this IP in your browser:");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", []() {
+    String html = "<html><body style='font-family:sans-serif;text-align:center;margin-top:50px'>";
+    html += "<h2>ESP8266 Messenger</h2>";
+    html += "<p><b>Last received:</b> <span id='msg'>...</span></p>";
+    html += "<input id='input' type='text' placeholder='Type a message...' />";
+    html += "<button onclick=\"fetch('/send?msg='+encodeURIComponent(document.getElementById('input').value))\">Send</button>";
+    html += "<script>setInterval(()=>fetch('/receive').then(r=>r.text()).then(t=>document.getElementById('msg').innerText=t),1000);</script>";
+    html += "</body></html>";
+    server.send(200, "text/html", html);
+  });
+
+  server.on("/send", []() {
+    lastReceived = server.arg("msg");
+    Serial.println("Browser says: " + lastReceived);
+    server.send(200, "text/plain", "OK");
+  });
+
+  server.on("/receive", []() {
+    server.send(200, "text/plain", lastReceived);
+  });
+
+  server.begin();
 }
 
 void loop() {
-  if (bt.available()) {
-    char cmd = bt.read();
-    if (cmd == 'E') move(1, 0);      // Extend
-    else if (cmd == 'C') move(0, 1); // Contract
-    else if (cmd == 'S') move(0, 0); // Stop
+  server.handleClient();
+
+  if (Serial.available()) {
+    lastReceived = Serial.readString();
+    Serial.println("Message set to: " + lastReceived);
   }
 }
 ***/
